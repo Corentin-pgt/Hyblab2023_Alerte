5d12c4d80ac0b84522a79b0a019092cebc63fd05
Corentin <corentin.pinguet@etu.univ-nantes.fr>
2023-02-01 14:28:00 +0100

    Init db

--- alerte/server.js
+++ alerte/server.js
@@ -5,11 +5,27 @@
 const express = require('express');
 const path = require('path');
 
+// DB
+const sqlite3 = require('sqlite3').verbose();
+let db = new sqlite3.Database('./alerte/myDB.db', (err) => {
+    if (err) {
+        return console.error(err.message);
+    }
+    console.log('Connected to SQlite database.');
+});
+
+db.serialize(function () {
+    db.run("CREATE TABLE IF NOT EXISTS QCM (id INTEGER PRIMARY KEY, r1 Reponse)")
+    db.run("CREATE TABLE IF NOT EXISTS Reponse (id INTEGER PRIMARY KEY, libelle VARCHAR )")
+})
+
+
 // Create our application
 const app = express();
 
 // Load and register our REST API
 const api = require('./api/api');
+const {OPEN_CREATE} = require("sqlite3");
 app.use('/api', api);
 
 // Minimum routing: serve static content from the html directory
